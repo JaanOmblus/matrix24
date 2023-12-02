2620961fadcb6b7aca384af99153618609aca686
kvartiil <138615442+kvartiil@users.noreply.github.com>
2023-12-02 19:28:43 +0200

    Initial commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  //production: false,
+  //myCustomProperty: 'original value',
+  modelStruktuur: 0.65,
+  modelMaksevoime: 0.6,
+  modelRent: 0.3,
+  modelEfekt: 0.7,
+  modelLaen: 0.8
+};
+
+export const myNewConstant = {
+  key: 'value'
+};
